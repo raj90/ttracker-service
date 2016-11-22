64e12fda50160a58e36baff53e504373ac0352c1
Rajneesh <mrajneesh@telekha.com>
2016-11-22 23:44:34 +0530

    init1

---
+++ src/main/java/com/telekha/ttracker/TtrackerApplication.java
@@ -0,0 +1,12 @@
+package com.telekha.ttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/telekha/ttracker/TtrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.telekha.ttracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TtrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
